9ea920cbbd44beda60d0f7eb8ee03c49ec5e45ab
AR123456 <coloradoz99@gmail.com>
2018-05-06 20:08:52 -0600

    updates to user created questions

--- client/src/pages/CreateGame.js
+++ client/src/pages/CreateGame.js
@@ -1,144 +1,126 @@
-import React from "react";
+import React, { Component } from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import DeleteBtn from "../components/DeleteBtn";
+import APIdb from "../utils/APIdb";
 import Jumbotron from "../components/Jumbotron";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import { List, ListItem } from "../components/List";
 import { Link } from "react-router-dom";
-const CreateGame = () => (
-  <Container style={{ marginTop: 30 }}>
-    <Row>
-      <Col size="md-6">
-        <Jumbotron>
-          <h2>Add new questions</h2>
-        </Jumbotron>
-        <form>
-          <Input
-            onChange={this.handleInputChange}
-            name="question"
-            placeholder="Question (required)"
-          />
-          <Input
-            onChange={this.handleInputChange}
-            name="correct_answer"
-            placeholder="Correct answer (required)"
-          />
-          <Input
-            onChange={this.handleInputChange}
-            name="incorrect_1"
-            placeholder="Incorrect Answer One   (required)"
-          />
-          <Input
-            onChange={this.handleInputChange}
-            name="incorrect_2"
-            placeholder="Incorrect Answer Two(required)"
-          />
-          <Input
-            onChange={this.handleInputChange}
-            name="incorrect_3"
-            placeholder="Incorrect Answer Three (required)"
-          />
-          <FormBtn onClick={this.handleFormSubmit}>Submit Question </FormBtn>
-        </form>
-      </Col>
-      <Col size="md-6 sm-12">
-        <Jumbotron>
-          <h2>My Game Questions</h2>
-        </Jumbotron>
-        <h3>No Results to Display</h3>
-        {/* {this.state.questions.length ? (
-          <List>
-            {this.state.questions.data.map(question => (
-              <ListItem key={question._id}>
-                <Link to={"/questions/" + question._id}>
-                  <strong>{question}</strong>
-                </Link>
-                <DeleteBtn onClick={() => this.deleteQuestion(question._id)} />
-              </ListItem>
-            ))}
-          </List>
-        ) : (
-          <h3>No Results to Display</h3>
-        )} */}
-      </Col>
-    </Row>
-  </Container>
-);
 
+class CreateGame extends Component {
+  //setting the components initial state
+  state = {
+    category: "Mine",
+    type: "multiple",
+    difficulty: "medium",
+    question: "",
+    correct_answer: "",
+    incorrect_answers: ["", "", ""],
+    date: new Date(Date.now())
+  };
+  // When the component mounts, load all the DB questions to this.state.quizzes
+  componentDidMount() {
+    this.loadQuestions();
+  }
+  //loads all the quiz questions and sets them to this.state.quizzes
+  loadQuestions = () => {
+    APIdb.getQuizes()
+      .then(res => this.setState({ question: res.data }))
+      .catch(err => console.log(err));
+  };
+  //Deletes a question from the db with a given ID, then reloades teh quesitons from the db
+  deleteQuetion = id => {
+    APIdb.deleteQuiz(id)
+      .then(res => this.loadQuestions())
+      .catch(err => console.log(err));
+  };
+  //Handles updating component state when the user types into the input field
+  handleInputChange = event => {
+    const { question, value } = event.target;
+    console.log("This is the handleInput state change", value);
+
+    this.setState({
+      [question]: value
+    });
+  };
+  //When form is submitted, use the APIdb.save method to save the qeusiton data  Then reload the questions to the database
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (
+      this.state.question &&
+      this.state.correct_answer &&
+      this.state.incorrect_answers
+    ) {
+      APIdb.saveQuiz({
+        category: "Mine",
+        type: "multiple",
+        difficulty: "medium",
+        question: this.state.question,
+        correct_answer: this.state.correct_answer,
+        incorrect_answers: this.state[
+          (this.state.incorrect_1,
+          this.state.incorrect_2,
+          this.state.incorrect_3)
+        ]
+      })
+        .then(res => this.loadQuestions())
+        .catch(err => console.log(err));
+      console.log("This is the handale form submit");
+    }
+    // send me to the db
+    // this.props.createNewQuestion();
+  };
+  render() {
+    console.log("I was triggered during render");
+    return (
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h2>Add new questions</h2>
+            </Jumbotron>
+            <form>
+              <Input
+                onChange={this.handleInputChange}
+                name="question"
+                placeholder="Question (required)"
+              />
+              <Input
+                onChange={this.handleInputChange}
+                name="correct_answer"
+                placeholder="Correct answer (required)"
+              />
+              <Input
+                onChange={this.handleInputChange}
+                name="incorrect_1"
+                placeholder="Incorrect Answer One   (required)"
+              />
+              <Input
+                onChange={this.handleInputChange}
+                name="incorrect_2"
+                placeholder="Incorrect Answer Two(required)"
+              />
+              <Input
+                onChange={this.handleInputChange}
+                name="incorrect_3"
+                placeholder="Incorrect Answer Three (required)"
+              />
+              <FormBtn onClick={this.handleFormSubmit}>
+                Submit Question{" "}
+              </FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h2>My Game Questions</h2>
+            </Jumbotron>
+            <h3>No Results to Display</h3>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 export default CreateGame;
-// code from work with ben use this for the create user entry form
-// class CreateForm extends Component {
-//   constructor() {}
-
-//   onChange = e => {
-//     const { name, value } = e.target;
-
-//     this.setState({
-//       [name]: value
-//     });
-//   };
-
-//   formatQuestion = e => {
-//     e.preventDefault();
-//     const {
-//       category,
-//       difficulty,
-//       question,
-//       correct_answer,
-//       incorrect_1,
-//       incorrect_2,
-//       incorrect_3
-//     } = this.state;
-
-//     const question = {
-//       category: "Mine",
-//       type: "multiple",
-//       difficulty: "medium",
-//       question,
-//       correct_answer,
-//       incorrect_answers: [incorrect_1, incorrect_2, incorrect_3],
-//       date: new Date(Date.now())
-//     };
-
-//     // send me to the db
-//     // this.props.createNewQuestion(question);
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         {/* <div className="form text-center">{props.children}</div> */}
-
-//         <form>
-//           <input name="category" placeholder="Category  (required)" />
-//           <input name="difficulty" placeholder="Difficulty  (required)" />
-//           <input name="question" placeholder="Question  (required)" />
-//           <input
-//             name="correct_answer"
-//             placeholder="Correct answer (required)"
-//           />
-//           <input
-//             name="incorrect_1"
-//             placeholder="incorrect answer one  (required)"
-//           />
-//           <input
-//             name="incorrect_2"
-//             placeholder="incorrect answer two  (required)"
-//           />
-//           <input
-//             name="incorrect_3"
-//             placeholder="incorrect answer three (required)"
-//           />
-//           {/* <TextArea name="synopsis" placeholder="Synopsis (Optional)" /> */}
-
-//           {/* this button need to have this or this type of on click in it onClick={formatQuestion} */}
-//           <button>Add this question to the database</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// export default CreateForm;
--- controllers/quizesController.js
+++ controllers/quizesController.js
@@ -5,28 +5,28 @@ module.exports = {
   findAll: function(req, res) {
     db.Quiz.find(req.query)
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
+      .then(dbQuiz => res.json(dbQuiz))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     db.Quiz.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
+      .then(dbQuiz => res.json(dbQuiz))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     db.Quiz.create(req.body)
-      .then(dbModel => res.json(dbModel))
+      .then(dbQuiz => res.json(dbQuiz))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     db.Quiz.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
+      .then(dbQuiz => res.json(dbQuiz))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
     db.Quiz.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
+      .then(dbQuiz => dbQuiz.remove())
+      .then(dbQuiz => res.json(dbQuiz))
       .catch(err => res.status(422).json(err));
   }
 };
