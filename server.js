66a37438ad8d83f57ed19c082fdf3c37ace18d99
AR123456 <coloradoz99@gmail.com>
2018-05-07 19:52:25 -0600

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const MONGODB_URI = require("./config/keys");
+const MONGODB_URI = require("./config/keys");
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
