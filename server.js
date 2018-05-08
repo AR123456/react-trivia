e0f54d56e8562093421b237d52d749c9ee1983b6
AR123456 <coloradoz99@gmail.com>
2018-05-07 19:42:36 -0600

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = require("./config/keys");
+// const MONGODB_URI = require("./config/keys");
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,18 +13,23 @@ app.use(bodyParser.json());
 app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
-
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost/reacttrivia");
+  console.log("mongoose connected");
+}
 //if deployed use deployed db , other wise use the local mongoheadlines db
-var db = MONGODB_URI || "mongodb://localhost/reacttrivia";
+// var db = MONGODB_URI || "mongodb://localhost/reacttrivia";
 //connect mongoose to the db
-mongoose.connect(db, function(error) {
-  //log error if it happens
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("mongoose connected");
-  }
-});
+// mongoose.connect(db, function(error) {
+//   //log error if it happens
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("mongoose connected");
+//   }
+// });
 
 // Start the API server
 app.listen(PORT, function() {
